fbe7b874282d10b07bcc3703463966c19cfc4beb
Mikhail Skobov <4219923+kail@users.noreply.github.com>
2020-11-23 07:54:55 -0800

    Adding high level interface from meeting (#1)
    * Adding high level interface from meeting
    
    * whoops
    
    * Remove whitespace, rename vote fxn to not conflict with attribute
    
    Co-authored-by: Keyan Pishdadian <kpishdadian@gmail.com>

--- src/proof_server.py
+++ src/proof_server.py
@@ -1,9 +1,41 @@
+from src import sv_vote
+
+
 class ProofServer:
-    def __init__(self):
+    def __init__(self, rows: int):
+        self._generate_key_pair()
+        self._rows = rows
+
+    #
+    # Proof server initialization
+    #
+    def _generate_key_pair(self):
+        self._public_key = ''
+        self._secret_key = ''
+
+    #
+    # Public proof server functions/endpoints
+    #
+    def register_tablet(self, tablet_id: str, pk_encrypted_sk):
         pass
 
+    def get_public_key(self):
+        return self._public_key
+
+    def get_num_rows(self) -> int:
+        return self._rows
+
+    def handle_vote(self, sv_vote: sv_vote.SVVote):
+        pass
+
+    #
+    # Proof server mixing implementation
+    #
     def _publish_proof(self):
         pass
 
     def mix_votes(self):
         pass
+
+
+
--- src/voter.py
+++ src/voter.py
@@ -1,7 +1,17 @@
+from src.tablet import Tablet
+from src.sbb import SBB
+from src.sv_vote import SVVote
+
+
 class Voter:
     def __init__(self):
         self.ballot_hash = None
         self.bid = None
+        # TODO: actually create a valid vote
+        self.vote = SVVote()
 
-    def vote(self):
-        pass
+    def do_vote(self, tablet: Tablet):
+        self.bid, self.ballot_hash = tablet.send_vote(self.vote)
+
+    def verify(self, sbb: SBB) -> bool:
+        return True

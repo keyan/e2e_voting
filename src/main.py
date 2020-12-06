6f0006a61c5df7d523b9806c15c64fffae794d8d
Keyan Pishdadian <kpishdadian@gmail.com>
2020-12-06 14:11:30 -0800

    Remove interactive mode, which was never implemented

--- src/main.py
+++ src/main.py
@@ -8,14 +8,6 @@ def parse_cmd_line(args):
     parser = argparse.ArgumentParser(description='Run simulated electronic elections')
 
     parser.add_argument(
-        '-i', '--interactive',
-        action='store_true',
-        dest='interactive',
-        default=False,
-        help='start simulation in interactive mode, allowing user control of some election parameters like voting',
-    )
-
-    parser.add_argument(
         '-v', '--voters',
         action='store',
         dest='num_voters',
@@ -29,7 +21,4 @@ def parse_cmd_line(args):
 
 if __name__ == '__main__':
     parsed = parse_cmd_line(sys.argv[1:])
-    if parsed.interactive:
-        print('Interactive mode not implemented yet')
-    else:
-        election.Election(num_voters=parsed.num_voters).run()
+    election.Election(num_voters=parsed.num_voters).run()
